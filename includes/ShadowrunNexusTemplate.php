e083b414f6260ab34beb261be0780eebeefcc423
catrone3 <catrone3@gmail.com>
2025-03-25 13:48:27 +0000

    see if these changes work on the wiki layout

--- includes/ShadowrunNexusTemplate.php
+++ includes/ShadowrunNexusTemplate.php
@@ -347,14 +347,16 @@ class ShadowrunNexusTemplate extends BaseTemplate {
                 </div>
                 
                 <!-- Add Ko-fi Support Link -->
-                <div class="sr-nexus-kofi">
-                    <a href="https://ko-fi.com/catrone3" target="_blank" rel="noopener noreferrer">
-                    <svg class="sr-nexus-kofi-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2 2" fill="white">
-                        <path d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734 4.352.24 7.422-2.831 6.649-6.916zm-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09-.443-.441-3.368-3.049-4.034-3.954-.709-.965-1.041-2.7-.091-3.71.951-1.01 3.005-1.086 4.363.407 0 0 1.565-1.782 3.468-.963 1.904.82 1.832 3.011.723 4.311zm6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015z"/>
-                    </svg>
-                    Keep this site ad free by supporting me on Ko-fi
-                    </a>
-                </div>
+                <div class="sr-nexus-kofi" style="text-align: center; margin-top: 0.5rem; padding-top: 0.5rem; border-top: 1px solid #333333; width: 100%; display: block;">
+					<div style="max-width: 150px; margin: 0 auto; display: block;">
+					<a href="https://ko-fi.com/yourusername" target="_blank" rel="noopener noreferrer" style="display: inline-flex; align-items: center; background-color: #13C3FF; color: #fff; padding: 0.3rem 0.8rem; border-radius: 4px; font-weight: bold; font-size: 0.9rem; text-decoration: none; width: auto;">
+						<svg style="margin-right: 0.4rem; width: 16px; height: 16px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
+						<path d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734 4.352.24 7.422-2.831 6.649-6.916zm-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09-.443-.441-3.368-3.049-4.034-3.954-.709-.965-1.041-2.7-.091-3.71.951-1.01 3.005-1.086 4.363.407 0 0 1.565-1.782 3.468-.963 1.904.82 1.832 3.011.723 4.311zm6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015z"/>
+						</svg>
+						Support on Ko-fi
+					</a>
+					</div>
+				</div>
                 </footer>
 		</div>
 		<?php 
